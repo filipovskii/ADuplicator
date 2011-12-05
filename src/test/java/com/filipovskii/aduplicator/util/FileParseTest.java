d17e7aa49918dce3ef9330d9803636bdf5a35f7e
filipovskii_off <filipovskii.off@gmail.com>
2011-12-06 03:58:38 +0400

    working algorithm

--- src/test/java/com/filipovskii/aduplicator/util/FileParseTest.java
+++ src/test/java/com/filipovskii/aduplicator/util/FileParseTest.java
@@ -5,14 +5,18 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.File;
 import java.io.StreamTokenizer;
+import java.util.Scanner;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+/**
+ * Set of tests for understanding different file parse methods
+ */
 public class FileParseTest {
   
   @Test
-  public void readChunkByChunk() throws Exception {
+  public void streamReader() throws Exception {
     Reader reader = new BufferedReader(
         new FileReader(new File("src/test/resources/test-data.txt")));
     StreamTokenizer tok = new StreamTokenizer(reader);
@@ -23,5 +27,20 @@ public class FileParseTest {
       System.out.println(tok.sval);
     }
     reader.close();
+  }
+
+  @Test
+  public void scanner() throws Exception {
+    Reader reader = new BufferedReader(
+        new FileReader(new File("src/test/resources/test-data.txt")));
+    Scanner sc = new Scanner(reader);
+    // it is posible to use nextLine() method instead
+    sc.useDelimiter("\n");
+    while (sc.hasNext()) {
+      String l = sc.next();
+      assertTrue(l != null);
+      assertTrue(l.length() > 0);
+      System.out.println(l);
+    }
   }
 }
